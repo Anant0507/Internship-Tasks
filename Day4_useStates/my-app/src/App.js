cdde4f3d63a6019661d6155be7518fb2be22028a
Anant Patel <anant.patel4762@gmail.com>
2023-05-23 11:32:20 +0530

    Day 4 task related to useStates

---
+++ Day4_useStates/my-app/src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Home } from './components/Home';
+import { User } from './components/User';
+import { Routes,Route, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from "./style.js"
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div>
+
+          <Link to="/" style={{marginLeft:5}}>Home</Link>
+          <Link to="/user" style={{marginLeft:10}}>User</Link>
+
+        </div>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/user" element={<User/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
